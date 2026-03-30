9d129eae9bb1459ddba306cbd85787184b295b89
EunsooSoh <no1_0218@naver.com>
2026-03-30 22:22:21 +0900

    [feat] 카테고리 옵션에서 식사 추가

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,12 +3,13 @@
  * Licensed under the MIT License.
  */
 
-export const CATEGORIES = ['운동', '공부', '개발', '독서', '기타'] as const;
+export const CATEGORIES = ['운동', '공부', '개발', '독서', '식사', '기타'] as const;
 
 export const CATEGORY_COLORS: { [key: string]: string } = {
   '운동': 'bg-green-600',
   '공부': 'bg-blue-600',
   '개발': 'bg-purple-600',
   '독서': 'bg-yellow-600',
+  '식사': 'bg-orange-600',
   '기타': 'bg-gray-600',
 };
